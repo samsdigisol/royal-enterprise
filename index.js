3ca9366371399803bc76130d23407d33987b72de
unknown <samsdigitalsolution@outlook.com>
2023-10-06 08:58:46 +0530

    first commit

---
+++ index.js
@@ -0,0 +1,24 @@
+function contactTo(){
+    var name = document.getElementById("autoSizingInput").value;
+    var mail = document.getElementById("autoSizingInputGroup").value;
+    var ser = document.getElementById("autoSizingSelect").value;
+
+    name = name.replace(/\s/g,"%20");
+    ser = ser.replace(/\s/g,"%20");
+
+    var url = "https://wa.me/+919823891852?text="+name+"%0a"+mail+"%0a"+ser;
+    window.open(url , "_blank");
+}
+function getQuote(ser){
+    ser = ser.replace(/\s/g,'%20');
+    var url = "https://wa.me/+919823891852?text=I%20Want%20To%20Know%20More%20About%20+"+ser;
+    window.open(url , "_blank");
+}
+function scrollSer(){
+    const scrollDiv = document.getElementsById("ser");
+    scrollDiv.scrollIntoView();
+}
+function scrollCon(){
+    const scrollDiv = document.getElementById("con");
+    scrollDiv.scrollIntoView();
+}
